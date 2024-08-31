852607ae0c5a8e5466fc01a267dc5531c396e30b
saurabhkushwahha <saurabhkushwahhaa@gmail.com>
2024-08-31 23:51:18 +0530

    changes in file

--- 08megaBlogAppwrite/src/components/index.js
+++ 08megaBlogAppwrite/src/components/index.js
@@ -1,13 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import container from './container/container'
-import logo from './Logo'
+import Logo from './Logo'
 import logoutBtn from "./Header/logoutBtn";
 export {
      Header,
      Footer,
      container,
-     logo,
+     Logo,
      logoutBtn,
      
      
